dbd6897e7fbaeb04c9476f7ec19f4105d64c3bff
Anastasia Botnar <botnar@goodsforecast.ru>
2024-12-13 22:14:32 +0300

    rewrite to pass tests

--- swapi.py
+++ swapi.py
@@ -1,54 +1,83 @@
-import os
+from pathlib import Path
 import requests
 
 
-def save_sw_data(url):
-    category_info_request = SWRequester(url)
-    os.makedirs('data', exist_ok=True)
-    categories = category_info_request.get_sw_categories()  # Список категорий
+def save_sw_data():
+    request = SWRequester('https://swapi.dev/api')
+    path = 'data'
+    Path(path).mkdir(exist_ok=True)
+    categories = request.get_sw_categories()  # Список категорий
+
+    # for item in categories_dict.keys():
+        #     categories.append(item)
+        # categories.sort()
+        # return categories
 
     for item in categories:
         # Информация о категории в виде строки
-        category_info = category_info_request.get_sw_info(item)
+        category_info = request.get_sw_info(item)
         file_name = item + '.txt'
-        file_path = 'data/' + file_name
+        file_path = f'{path}/{file_name}'
         with open(file_path, 'w') as file:
             file.write(category_info)
 
 
 class APIRequester:
-    def __init__(self, base_url):
-        if base_url[-1] != '/':
-            self.base_url = base_url + '/'
+    def __init__(self, base_url): # Базовый URL для swapi - это https://swapi.dev/
+        if base_url[-1] == '/':
+            self.base_url = base_url[0:-1]
         else:
             self.base_url = base_url
 
-    def get(self, url):
+    def get(self, relative_path=''):
+        if relative_path != '':
+            url = f'{self.base_url}{relative_path}'
+        else:
+            url = f'{self.base_url}'    
         try:
             response = requests.get(url)
             response.raise_for_status()
             return response
+        except requests.RequestException as e:
+            return e
         except requests.ConnectionError:
             return response
         except requests.HTTPError:
             return response
-        except requests.RequestException:
-            return response
         except requests.Timeout:
             return response
+        # except requests.RequestException as e:
+        #     return e
 
 
 class SWRequester(APIRequester):
+
     def get_sw_categories(self):
-        response = self.get(self.base_url)
-        categories = []
-        for item in response.json().keys():
-            categories.append(item)
-        return categories
+        """Попробовать добавить проверку на код 200 и только в этом случае
+        пытаться получить json"""
+        # categories_dict = {}
+        # categories = []
+        try:
+            response = self.get(relative_path='/')
+            response.raise_for_status()
+            # categories_dict = response.json()
+            return response.json().keys()
+        except requests.ConnectionError:
+            return response
+        except requests.HTTPError:
+            return response
+        except requests.RequestException as e:
+            return e
+        except requests.Timeout:
+            return response
+
+        # for item in categories_dict.keys():
+        #     categories.append(item)
+        # categories.sort()
+        # return categories
 
     def get_sw_info(self, sw_type):
-        category_info_url = self.base_url + sw_type + '/'
-        response = self.get(category_info_url)
+        response = self.get(relative_path=f'/{sw_type}/')
         return response.text
 
 
@@ -60,4 +89,10 @@ class SWRequester(APIRequester):
 # print(cat.get_sw_info('people'))  # Возвращает строку
 # print(type(cat.get_sw_info('people')))
 
-save_sw_data('https://swapi.dev/api/')
+
+save_sw_data()
+
+# print(m.base_url)
+# print(m.url)
+# print('-' * 5)
+# print(m.get_sw_categories())
